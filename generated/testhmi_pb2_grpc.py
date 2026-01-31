71378e8132d04937c64bec95900f4767fd0a45f6
steffen2308-ger <steffen2308@gmail.com>
2026-01-31 10:43:42 +0100

    Fix generated gRPC import

--- generated/testhmi_pb2_grpc.py
+++ generated/testhmi_pb2_grpc.py
@@ -4,7 +4,7 @@ import grpc
 import warnings
 
 from google.protobuf import empty_pb2 as google_dot_protobuf_dot_empty__pb2
-import openWriteStreamZuweisung_pb2 as openWriteStreamZuweisung__pb2
+from . import openWriteStreamZuweisung_pb2 as openWriteStreamZuweisung__pb2
 
 GRPC_GENERATED_VERSION = '1.76.0'
 GRPC_VERSION = grpc.__version__
